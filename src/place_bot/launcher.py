b97fd98315408a55c47d11e69d43acb871fe8657
Emmanuel Battesti <emmanuel.battesti@ensta-paris.fr>
2022-11-18 15:59:52 +0100

    remove save_data, score_manager and write_pdf

--- src/place_bot/launcher.py
+++ src/place_bot/launcher.py
@@ -1,6 +1,4 @@
 from solutions.my_robot_eval import MyRobotEval
-from spg_overlay.utils.score_manager import ScoreManager
-from spg_overlay.utils.save_data import SaveData
 from spg_overlay.utils.screen_recorder import ScreenRecorder
 from spg_overlay.utils.team_info import TeamInfo
 from spg_overlay.gui_map.gui_sr import GuiSR
@@ -8,6 +6,7 @@ from spg_overlay.gui_map.gui_sr import GuiSR
 from maps.map_intermediate_01 import MyMapIntermediate01
 from maps.map_complete_01 import MyMapComplete01
 from maps.map_complete_02 import MyMapComplete02
+
 
 # from solutions.my_robot_random import MyRobotRandom
 
@@ -35,26 +34,15 @@ class Launcher:
         self.number_wounded_persons = my_map.number_wounded_persons
         self.size_area = my_map.size_area
 
-        self.score_manager = ScoreManager(number_robots=self.number_robots,
-                                          time_step_limit=self.time_step_limit,
-                                          real_time_limit=self.real_time_limit,
-                                          total_number_wounded_persons=self.number_wounded_persons)
-
-        self.save_data = SaveData(self.team_info, disabled=False)
         self.real_time_limit_reached = False
-        self.video_capture_enabled = True
-        self.video_capture_enabled &= not self.save_data.disabled
+        self.video_capture_enabled = False
 
-    def one_round(self, num_round):
+    def go(self):
         my_map = MyMap()
         playground = my_map.construct_playground(robot_type=MyRobot)
 
-        num_round_str = str(num_round)
-        team_number_str = str(self.team_info.team_number).zfill(2)
         if self.video_capture_enabled:
-            filename_video_capture = self.save_data.path + \
-                                     "/screen_rd{}_eq{}.avi".format(num_round_str,
-                                                                    team_number_str)
+            filename_video_capture = self.save_data.path + "/screen.avi".format()
         else:
             filename_video_capture = None
 
@@ -66,55 +54,6 @@ class Launcher:
         my_map.explored_map.reset()
 
         my_gui.run()
-
-        score_exploration = my_map.explored_map.score()
-
-        last_image_explo_lines = my_map.explored_map.get_pretty_map_explo_lines()
-        last_image_explo_zones = my_map.explored_map.get_pretty_map_explo_zones()
-        self.save_data.save_images(my_gui.last_image,
-                                   last_image_explo_lines,
-                                   last_image_explo_zones,
-                                   num_round)
-
-        return my_gui.elapsed_time, my_gui.rescued_all_time_step, score_exploration, my_gui.rescued_number, my_gui.real_time_elapsed
-
-    def go(self):
-        print("")
-        for i_try in range(self.nb_rounds):
-            result = self.one_round(i_try)
-            (elapsed_time_step, rescued_all_time_step, score_exploration,
-             rescued_number, real_time_elapsed) = result
-
-            result_score = self.score_manager.compute_score(rescued_number,
-                                                            score_exploration,
-                                                            rescued_all_time_step)
-            (final_score, percent_rescued, score_time_step) = result_score
-
-            print("\t* Round n°{}".format(i_try),
-                  ", real time elapsed = {:.1f}/{}s".format(
-                      real_time_elapsed, self.real_time_limit),
-                  ", rescued number ={}/{}".format(
-                      rescued_number, self.number_wounded_persons),
-                  ", exploration score =", "{:.1f}%".format(
-                    score_exploration * 100),
-                  ", elapse time = {}/{} steps".format(
-                      elapsed_time_step, self.time_step_limit),
-                  ", time to rescue all = {} steps".format(
-                      rescued_all_time_step),
-                  ", final score =", "{:.2f}/100".format(final_score)
-                  )
-            if self.real_time_limit_reached:
-                print("\t\tThe real time limit of {}s is reached first.".format(
-                    self.real_time_limit))
-
-            self.save_data.save_one_round(i_try,
-                                          percent_rescued,
-                                          score_exploration,
-                                          elapsed_time_step,
-                                          rescued_all_time_step,
-                                          score_time_step,
-                                          final_score)
-        self.save_data.fill_pdf()
 
 
 if __name__ == "__main__":
