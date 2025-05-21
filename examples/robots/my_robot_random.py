1f0aaf6e47771e76335f9f7001514315bf1c84f5
Emmanuel Battesti <emmanuel.battesti@ensta-paris.fr>
2025-05-21 21:12:16 +0200

    improve example my_robot_random

--- examples/robots/my_robot_random.py
+++ examples/robots/my_robot_random.py
@@ -4,56 +4,81 @@ The Robot will move forward and turn for a random angle when an obstacle is hit
 """
 import math
 import random
+from enum import Enum
 
 from place_bot.entities.robot_abstract import RobotAbstract
 from place_bot.utils.utils import normalize_angle
 
 
+class State(Enum):
+    STRAIGHT = 1
+    TURN = 2
+
+
 class MyRobotRandom(RobotAbstract):
     def __init__(self):
         super().__init__(should_display_lidar=False)
-        self.counterStraight = 0
-        self.angleStopTurning = random.uniform(-math.pi, math.pi)
-        self.distStopStraight = random.uniform(10, 50)
-        self.isTurning = False
+        self.counter_straight = random.randint(50, 100)
+        self.target_angle = random.uniform(-math.pi, math.pi)
+        self.state = State.STRAIGHT
+        self.pause_after_turn = 0
 
     def process_lidar_sensor(self):
-        if self.lidar().get_sensor_values() is None:
+        """Detects if an obstacle is closer than a threshold."""
+        sensor_values = self.lidar().get_sensor_values()
+        if sensor_values is None:
             return False
-
-        min_distance = min(self.lidar().get_sensor_values())
-
-        return min_distance < 30
+        return min(sensor_values) < 30
 
     def control(self):
         """
-        The Robot will move forward and turn for a random angle when an obstacle is hit
+        The Robot will move forward and turn for a random angle when an obstacle is hit.
         """
-        command_straight = {"forward": 1.0,
-                            "rotation": 0.0}
-
-        command_turn = {"forward": 0.0,
-                        "rotation": 1.0}
+        command_straight = {"forward": 1.0, "rotation": 0.0}
+        command_turn_left = {"forward": 0.0, "rotation": 1.0}
+        command_turn_right = {"forward": 0.0, "rotation": -1.0}
 
         touched = self.process_lidar_sensor()
-
-        self.counterStraight += 1
-
-        if touched and not self.isTurning and self.counterStraight > self.distStopStraight:
-            self.isTurning = True
-            self.angleStopTurning = random.uniform(-math.pi, math.pi)
-
         measured_angle = 0
         if self.true_angle() is not None:
             measured_angle = self.true_angle()
 
-        diff_angle = normalize_angle(self.angleStopTurning - measured_angle)
-        if self.isTurning and abs(diff_angle) < 0.2:
-            self.isTurning = False
-            self.counterStraight = 0
-            self.distStopStraight = random.uniform(10, 50)
+        diff_angle = normalize_angle(self.target_angle - measured_angle)
 
-        if self.isTurning:
-            return command_turn
+        if (self.state == State.STRAIGHT and
+                ((self.pause_after_turn == 0 and touched) or
+                 self.counter_straight == 0)):
+            # If the robot is go straight and
+            #    it has touched an obstacle
+            # or it has moved enough distance, then it will turn.
+
+            # Transition to TURN state
+            print(f"STRAIGHT => TURN, touched = {touched}")
+            self.state = State.TURN
+            # We set the target angle to a random value between -pi and pi
+            self.target_angle = random.uniform(-math.pi, math.pi)
+
+        elif self.state == State.TURN and abs(diff_angle) < 0.2:
+            # If the robot is turning and it has turned enough
+            # then it will go straight
+
+            # Transition to STRAIGHT state
+            print(f"TURN => STRAIGHT, diff = {diff_angle:.2f}")
+            self.state = State.STRAIGHT
+            self.pause_after_turn = 15
+            self.counter_straight = random.randint(50, 100)
+
+        if self.state == State.TURN:
+            print(f"Turning, diff = {diff_angle:.2f}")
+            if diff_angle > 0:
+                return command_turn_left
+            else:
+                return command_turn_right
         else:
+            if self.counter_straight > 0:
+                self.counter_straight -= 1
+            if self.pause_after_turn > 0:
+                self.pause_after_turn -= 1
+            print(
+                f"Straight, touched = {touched}, counter = {self.counter_straight}, counterTurns = {self.pause_after_turn}")
             return command_straight
